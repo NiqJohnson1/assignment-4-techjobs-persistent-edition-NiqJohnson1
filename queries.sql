bef916a9781fc60c9a44df9a029702c8513026d4
Niquelle <niqjohnson1@gmail.com>
2021-11-30 16:08:03 -0500

    Connected application to MYSQL database and added annotations to the Abstract Entity

--- queries.sql
+++ queries.sql
@@ -1,4 +1,11 @@
 -- Part 1: Test it with SQL
+--    CREATE TABLE job (
+--    id INTEGER PRIMARY KEY AUTO_INCREMENT,
+--    name VARCHAR (50),
+--    employer VARCHAR (80),
+--    skills VARCHAR (80)
+--    );
+
 
 -- Part 2: Test it with SQL
 
